3a12933d6647fd220277ed6c691d9e4d542cbbc0
Yuhee2020 <62483987+Yuhee2020@users.noreply.github.com>
2022-02-22 19:55:46 +0300

    operstors

---
+++ bogdan/operators.js
@@ -0,0 +1,17 @@
+const buttonInfo = {
+    text: "Buy"
+}
+
+
+const buttonStyle = {
+    color : "yellow",
+    widtg : 200,
+    height: 300
+}
+
+const button = {
+    ...buttonInfo,
+    ...buttonStyle
+}
+
+console.table(button)
