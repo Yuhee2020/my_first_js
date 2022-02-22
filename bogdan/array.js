e58631ea411a02729f2ea4cbf1e386164af7ea5d
Yuhee2020 <62483987+Yuhee2020@users.noreply.github.com>
2022-02-22 22:31:02 +0300

    arrays

---
+++ bogdan/array.js
@@ -0,0 +1,11 @@
+const myArray = [1,2,4,5,6,7,8]
+console.log(myArray)
+
+const myArray2 = new Array(1,2,3,4,5,5,6,7)
+console.log(myArray2)
+
+myArray2.forEach(function(el){
+    console.log(el*2)
+})
+
+myArray.forEach(i => console.log(i*3))
--- bogdan/errors.js
+++
@@ -1,5 +0,0 @@
-const fnWithError = ()=> {
-    throw new Error ("Some error")
-}
-fnWithError()
-console.log("Continue...")
