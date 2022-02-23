cefea831cbfba000bfcabfaab8b1078b3f46d566
Yuhee2020 <62483987+Yuhee2020@users.noreply.github.com>
2022-02-23 22:00:52 +0300

    ifelse in function

--- bogdan/ifelse.js
+++ bogdan/ifelse.js
@@ -1,8 +1,24 @@
-const age = 20
-if (age>18){
+const age = 25
+if (age>=18){
     console.log("Is adult")
-} else if (age>=12){
+} 
+
+if (age>=12 && age<18 ){
     console.log("teenager")
-} else {
-    console.log("Is child")
 }
+
+if (age<12){
+        console.log("Is child")
+    }
+
+
+const sumPositiveNumb = (a,b) => {
+    if (typeof a !== "number" || typeof b !=="number"){
+        return "One of the arguments is a not number"
+    }
+    if(a<=0 || b<=0){
+        return "Numbers are not positive"
+    }
+    return  a + b
+}
+console.log(sumPositiveNumb(true,10))
