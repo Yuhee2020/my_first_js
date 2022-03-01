768c999d6d10db938d9e52fb067e2b35634da389
Yuhee2020 <62483987+Yuhee2020@users.noreply.github.com>
2022-03-01 20:43:20 +0300

    create

---
+++ bogdan/new copy.js
@@ -0,0 +1,10 @@
+const myCity = {
+    city: "New York",
+    popular: true,
+    country: "USA"
+}
+console.log(myCity.city)
+myCity.city = "Las Vegas"
+console.log(myCity.city)
+myCity.continent = "America"
+console.log(myCity)
