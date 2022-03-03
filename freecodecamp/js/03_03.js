affc2ca2839a04d0f7c90bdaa1da4b52fb2ca790
Yuhee2020 <62483987+Yuhee2020@users.noreply.github.com>
2022-03-03 22:02:27 +0300

    03_03

---
+++ freecodecamp/js/03_03.js
@@ -0,0 +1,19 @@
+const remainder = 11%2;
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a +=12;
+b +=9;
+c +=7;
+
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a *=5;
+b *= 3;
+c *=10;
