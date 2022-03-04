08a36753de93844d0f0bb89590348beeb1145f02
Yuhee2020 <62483987+Yuhee2020@users.noreply.github.com>
2022-03-04 22:41:45 +0300

    codewars

---
+++ codewars/1.js
@@ -0,0 +1,11 @@
+function findDifference(a, b) {
+    return Math.abs(a[0]*a[1]*a[2]-b[0]*b[1]*b[2])
+       } //модуль числа
+
+function multiply(a, b){
+    return  a * b;
+    }
+
+function removeChar(str){
+return str.substring(1, str.length-1);
+};   
