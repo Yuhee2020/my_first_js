1e6214a3bee25629e6ee60e47935a2d8dc21c90a
Yuhee2020 <62483987+Yuhee2020@users.noreply.github.com>
2022-03-07 21:26:31 +0300

    Remove an exclamation mark from the end of string

---
+++ codewars/Remove an exclamation mark from the end of string.js
@@ -0,0 +1,7 @@
+function remove (string) {
+    array1= string.split("")
+      if (array1[array1.length-1]==="!"){array1.pop();
+                                         return array1.join("")
+                                        }
+      else return string
+    }
