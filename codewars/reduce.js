763b62c08e0ea5f40276494754b9f972d5a7b031
Yuhee2020 <62483987+Yuhee2020@users.noreply.github.com>
2022-03-10 21:58:14 +0300

    reduce

--- codewars/reduce.js
+++ codewars/reduce.js
@@ -8,3 +8,7 @@ function find_average(array) {
   function arrayPlusArray(arr1, arr2) {
     return arr1.concat(arr2).reduce((a,b)=>a+b)
   }//еще редьюс
+
+  function grow(x){
+    return x.reduce((a,b)=>a*b)
+    }//еще
